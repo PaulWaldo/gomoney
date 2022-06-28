d64d9d98564d129eb4b1029528ba14591badfe5d
Paul Waldo <paul.w.waldo@gmail.com>
2022-06-28 18:01:21 -0400

    Updating Services test failure message

--- internal/db/db_test.go
+++ internal/db/db_test.go
@@ -4,13 +4,11 @@ import (
 	"testing"
 )
 
-func Test_NewSqliteInMemoryServices(t *testing.T) {
-	db, err := connectToDatabase("file::memory:?cache=shared")
+func Test_NewSqliteInMemoryServicesPopulatesDatabase(t *testing.T) {
+	_, err := NewSqliteInMemoryServices()
 	if err != nil {
-		t.Fatalf("Unable to create database: %s", err)
+		t.Fatalf("Error creating SqliteInMemoryServices: %s", err)
 	}
-	if db != nil {
-		t.Fatal("This is a test to see if test on gitlab cause failure")
-	}
-	// services := domain.Services{Account: NewAccountSvc(db)}
+	
+	t.Fatal("Need to use services to verify contents of database")
 }
