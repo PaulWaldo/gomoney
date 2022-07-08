42c72a15124a114e73090f56b1b99c28c7505f7a
Paul Waldo <paul.w.waldo@gmail.com>
2022-07-08 16:53:40 -0400

    Properly read transaction JSON

--- routes/transactions_test.go
+++ routes/transactions_test.go
@@ -8,6 +8,7 @@ import (
 
 	// "github.com/stretchr/testify/assert"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gorm.io/gorm"
 
@@ -27,25 +28,26 @@ func TestController_AddTransactionRoutes(t *testing.T) {
 
 	r := gin.Default()
 	controller := NewController(r, services)
-
-	// controller := Controller{
-	// 	router: gin.Default(),
-	// 	services: &domain.Services{
-	// 		Account: mocks.AccountSvc{},
-	// 		Transaction: ,
-	// 	},
-	// }
 	controller.AddTransactionRoutes()
+
 	w := httptest.NewRecorder()
 	req, err := http.NewRequest(http.MethodGet, constants.TransactionsURL, nil)
 	require.NoErrorf(t, err, "Got error creating request: %s", err)
 	controller.router.ServeHTTP(w, req)
+
 	require.Equal(t, http.StatusOK, w.Code, "expecting response code %d, got %d", http.StatusInternalServerError, w.Code)
-	var response map[string]interface{}
-	err = json.NewEncoder(w).Encode(response)
-	require.NoErrorf(t, err, "Got error encoding response: %s", err)
-	t.Logf("response=%v", response)
 	t.Logf("response body=%s", w.Body.String())
 
-	require.NotEmptyf(t, response, "output:\n%s", response)
+	// Convert JSON to map
+	var response []map[string]interface{}
+	// err = json.NewEncoder(w.Body).Encode(response)
+	err = json.Unmarshal([]byte(w.Body.Bytes()), &response)
+	require.NoErrorf(t, err, "Got error encoding response: %s", err)
+	t.Logf("response=%v", response)
+
+	for i := range transactions {
+		assert.Equal(t, transactions[i].Payee, response[i]["Payee"],
+			"expecting element %d's payee to be %s, but got %s",
+			transactions[i].Payee, response[i]["Payee"])
+	}
 }
