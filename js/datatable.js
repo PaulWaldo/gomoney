95e22e49e993940b20354cfac4285d0b9880ac9e
Paul Waldo <paul.w.waldo@gmail.com>
2022-07-15 11:58:22 -0400

    Adds datatable scroller

--- js/datatable.js
+++ js/datatable.js
@@ -2,6 +2,12 @@ $(document).ready(function () {
   $("#transactions").DataTable({
     serverSide: true,
     ajax: "/transactions",
+    scrollY: "50em",
+    deferRender: true,
+    // scroller: true,
+    scroller: {
+        displayBuffer: 20
+    },
     columns: [
       { data: "Payee" },
       // { data: "Type" },
