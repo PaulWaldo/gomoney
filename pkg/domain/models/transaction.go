2388007e91ddf04d0fb3510a53eb6ac3bb1ffc6f
Paul Waldo <paul.w.waldo@gmail.com>
2022-10-04 06:51:56 -0400

    Adds balance

--- pkg/domain/models/transaction.go
+++ pkg/domain/models/transaction.go
@@ -5,15 +5,13 @@ import (
 )
 
 type Transaction struct {
-	ID int64 `gorm:"primarykey" json:"id,omitempty"`
-	// CreatedAt time.Time      `json:"created_at,omitempty"`
-	// UpdatedAt time.Time      `json:"updated_at,omitempty"`
-	// DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
+	ID int64 
 	Payee     string    `json:"payee,omitempty"`
 	Type      string    `json:"type,omitempty"`
 	Amount    float64   `json:"amount,omitempty"`
 	Memo      string    `json:"memo,omitempty"`
 	Date      time.Time `json:"date,omitempty"`
-	AccountID int64     `json:"account_id,omitempty"`
+	Balance   float64
+	AccountID int64 `json:"account_id,omitempty"`
 	// TransferAccountId uint
 }
